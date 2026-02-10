e2beecb6323b4b886c18abc52077294212d55a0b
kalyanmewada <kalyanmewada08@gmail.com>
2026-02-10 21:56:55 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (44.47%) - LeetSync

---
+++ 792-binary-search/binary-search.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int s = 0;
+        int e = nums.size();
+        
+        while (s < e) {
+            int mid = s + (e - s) / 2;
+            
+            if (nums[mid] < target) {
+                s = mid + 1;
+            } else {
+                e = mid;
+            }
+        }
+        if (s < nums.size() && nums[s] == target)
+            return s;
+        
+        return -1;
+    }
+};
