510ec73940242fb8e4dcfc8fb4407172672361a5
kalyanmewada <kalyanmewada08@gmail.com>
2026-02-22 11:23:56 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (44.66%) - LeetSync

---
+++ 899-binary-gap/binary-gap.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int binaryGap(int n) {
+        int lastPosition = -1;
+        int maxDistance = 0;
+        int currentPosition = 0;
+
+        while (n > 0) {
+            if (n & 1) { 
+                if (lastPosition != -1) {
+                    maxDistance = max(maxDistance, 
+                                      currentPosition - lastPosition);
+                }
+                lastPosition = currentPosition;
+            }
+            n = n >> 1;   
+            currentPosition++;
+        }
+
+        return maxDistance;
+    }
+};
