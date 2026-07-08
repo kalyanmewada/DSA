d01fa3d1d75e03406ba6c00cf3c1a9bb4d013447
kalyanmewada <kalyanmewada08@gmail.com>
2026-07-08 23:59:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (7.82%) - LeetSync

---
+++ 258-add-digits/add-digits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int addDigits(int num) {
+
+        while (num >= 10) {
+
+            int sum = 0;
+
+            while (num > 0) {
+                sum += num % 10;   // Extract last digit
+                num /= 10;         // Remove last digit
+            }
+            
+            num = sum;
+        }
+
+        return num;
+    }
+};
